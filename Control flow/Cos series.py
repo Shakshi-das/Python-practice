240e6da8d8b0143610d68f25c5c5cceb27042beb
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-01-12 23:39:14 +0530

    Update Cos series.py

--- Control flow/Cos series.py
+++ Control flow/Cos series.py
@@ -1,6 +1,6 @@
 #program to calculate value of x in cosx series
 
-y=float(input("enter the value of x:"))
+x=float(input("enter the value of x:"))
 n=int(input("enter the value of n:"))
 x1=(3.14285*x)/180
 s=0
