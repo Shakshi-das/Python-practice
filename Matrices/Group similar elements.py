7887a42d55f82cc887ae91fc5894f9783d0b9783
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-02-15 23:09:32 +0530

    Update Group similar elements.py

--- Matrices/Group similar elements.py
+++ Matrices/Group similar elements.py
@@ -1,18 +1,15 @@
 #program to Group similar elements into Matrix
+
 from itertools import groupby
  
 # initializing list
-
 test_list = [1, 3, 5, 1, 3, 2, 5, 4, 2]
  
 # printing original list
-
 print("The original list : " + str(test_list))
  
 # Using list comprehension + groupby()
-
 res = [list(val) for key, val in groupby(sorted(test_list))]
  
 # printing result
-
 print("Matrix after grouping : " + str(res))
