0fb8e730a1047842aebf2673e207bf0e483850a6
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-05-24 23:42:17 +0530

    Update Sum of elements.py

--- Lists/Sum of elements.py
+++ Lists/Sum of elements.py
@@ -1,13 +1,11 @@
 #program to find sum of elements in a given list
 
 lst = []
-
 num = int(input('How many numbers: '))
 
 for n in range(num):
 
     numbers = int(input('Enter number '))
-
     lst.append(numbers)
 
 print("Sum of elements in given list is :", sum(lst))
