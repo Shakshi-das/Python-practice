67812dac8fda54a047b501add29bb2d78921521c
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-05-26 23:56:19 +0530

    Update Grades.py

--- Conditional statements/Grades.py
+++ Conditional statements/Grades.py
@@ -9,7 +9,6 @@ sub5=int(input("Enter marks of the fifth subject: "))
 total=(sub1+sub2+sub3+sub4+sub5)
 
 if(total>=90):
-
     print("Grade is outstanding")
 
 elif(total>=80):
@@ -37,5 +36,4 @@ elif:
     print("Fail")
 
 else:
-
     print("invalid input")
